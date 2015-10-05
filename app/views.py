c61022f7ece52d635107fa06e8ff03335f6dff19
OzKnightWalker <ozknightwalker62@gmail.com>
2015-10-05 13:25:36 -0700

    gone back to the original view

--- app/views.py
+++ app/views.py
@@ -2,5 +2,4 @@ from django.views import generic
 
 
 class MainView(generic.TemplateView):
-    # template_name = 'MainView.html'
-    template_name = 'TestingView.html'
+    template_name = 'MainView.html'
