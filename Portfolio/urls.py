39f5efa2aaa058301022a726ce28710a246ce4e3
OzKnightWalker <ozknightwalker62@gmail.com>
2015-10-05 10:41:22 -0700

    fix urls

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from app.views import MainView
 
 urlpatterns = [
-    url(r'^admin/$', include(admin.site.urls)),
-    url(r'/$', MainView.as_view()),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', MainView.as_view()),
 ]
