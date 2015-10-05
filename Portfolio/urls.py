702e0f5dfdece3981e3855546712c6523907944d
OzKnightWalker <ozknightwalker62@gmail.com>
2015-10-05 10:51:22 -0700

    customized admin side

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from app.views import MainView
 
+admin.site.site_header = 'Jc Din PortFolio Admin'
+admin.site.site_title = 'Jc Din PortFolio Admin'
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', MainView.as_view()),
