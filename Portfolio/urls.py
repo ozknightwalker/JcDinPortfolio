d6e90122ddd602dc5e08d0bf1275091d4300f3a4
OzKnightWalker <ozknightwalker62@gmail.com>
2015-10-26 10:49:32 -0700

    tried cleaning/organize some files

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from app.views import MainView
+from app import urls as app_urls
 
 admin.site.site_header = 'Jc Din PortFolio Admin'
 admin.site.site_title = 'Jc Din PortFolio Admin'
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', MainView.as_view()),
+    url(r'^', include(app_urls)),
 ]
