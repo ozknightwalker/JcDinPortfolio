d32199c4d60e61e7c0e0ebf3641bafbbb33507d2
OzKnightWalker <ozknightwalker62@gmail.com>
2015-10-26 10:44:56 -0700

    added test view

--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class MainView(generic.TemplateView):
     template_name = 'MainView.html'
+
+
+class TestView(generic.TemplateView):
+	template_name = 'TestingView.html'
