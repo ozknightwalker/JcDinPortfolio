8ea4ac195c4cbede53e91e013c84f66459f55b5a
ozknightwalker <ozknightwalker62@gmail.com>
2015-10-30 14:06:08 +0800

    made js to load all templates first for caching purposes

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@
     angular
         .module('app', ['ngRoute'])
         .config(['$routeProvider', '$locationProvider',
-            function($routeProvider, $locationProvider) {
+            function ($routeProvider, $locationProvider) {
                 $routeProvider
                     .when('/Profile', {
                         templateUrl: '/visit/Profile',
@@ -27,23 +27,76 @@
                     });
                    $locationProvider.html5Mode(true);          
         }])
-        .controller('ProfileCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location){
+        .controller('ProfileCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location) {
             $scope.$parent.menu_init();
         }])
-        .controller('ExperienceCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location){
+        .controller('ExperienceCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location) {
             $scope.$parent.menu_init();
         }])
-        .controller('AppCtrl', ['$http', '$scope', '$location', function ($http, $scope, $location) {
-            $scope.base_url = function(string){
-                if (string !== undefined){
+        .controller('AppCtrl', ['$http', '$scope', '$location', '$templateCache', function ($http, $scope, $location, $templateCache) {
+            $scope.base_url = function (string) {
+                if (string !== undefined) {
                     return $location.protocol() + '://'+ $location.host()+":"+ $location.port() + '/'+ string;
                 }else{
                     return $location.protocol() + '://'+ $location.host()+":"+ $location.port();
                 }
             };
-            $scope.loader = function(){
+            $scope.loader = function () {
                 console.log('loaded');
             };
+            $scope.alertError = function (title, message) {
+                swal(title, message, "error");
+            };
+            $scope.alertSuccess = function (title, message) {
+                swal(title, message, "success");
+            };
+            $http({
+                method : 'GET',
+                url : $scope.base_url('visit/Experience'),
+                cache : $templateCache
+            }).then(
+                function (response) {
+                    //Success
+                    $http({
+                        method : 'GET',
+                        url : $scope.base_url('visit/Profile'),
+                        cache : $templateCache
+                    }).then(
+                        function (response) {
+                            $http({
+                                method : 'GET',
+                                url : $scope.base_url('visit/Powers'),
+                                cache : $templateCache
+                            }).then(
+                                function(response){
+                                    $http({
+                                        method : 'GET',
+                                        url : $scope.base_url('visit/Works'),
+                                        cache : $templateCache
+                                    }).then(
+                                        function(response){},
+                                        function(response){
+                                            //Error
+                                            $scope.alertError('TemplateError', 'Loading Works encountered an error');
+                                        }
+                                    );
+                                },
+                                function(response){
+                                    //Error
+                                    $scope.alertError('Template Error', 'Loading Powers encountered an error');
+                                }
+                            );
+                        },
+                        function (response) {
+                            //Error
+                            $scope.alertError('Template Error!', 'Loading profile encountered an error');
+                        }
+                    );
+                },
+                function (response) {
+                    $scope.alertError('Template Error!', 'Loading experiences encountered an error');
+                }
+            );
             // Menu List
             $scope.menus = [
                 {
@@ -71,13 +124,13 @@
             }
             function mobilecheck() {
                 var check = false;
-                (function(a){if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
+                (function (a) {if(/(android|ipad|playbook|silk|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
                 return check;
             }
             $scope.animationVars = {
                 'property' : 1
             }
-            $scope.animationVarsInit = function(ev){
+            $scope.animationVarsInit = function (ev) {
                 // if ($scope.animationVars) {
                 //     console.log(ev.target);
                 //     console.log('null');
@@ -90,7 +143,7 @@
                 //     'contentWrapper' : this.container.querySelector( '.wrapper' );
                 // }
             }
-            $scope.menuclick = function(ev){
+            $scope.menuclick = function (ev) {
                 console.log(ev);
                 $scope.animationVarsInit(ev);
                 // ev.stopPropagation();
@@ -103,17 +156,17 @@
                 // // add modalview class
                 // classie.add( perspectiveWrapper, 'modalview' );
                 // // animate..
-                // setTimeout( function() { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
+                // setTimeout( function () { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
                 $scope.menuEnabled = true;
             }
-            $scope.ngViewClickEvent = function(){
+            $scope.ngViewClickEvent = function () {
                 if ($scope.menuEnabled) {
                     console.log('menu closing');
                     $scope.menuEnabled = false;
                 }
             }
             // Close Menu and start animation
-            $scope.close_menu = function(index){
+            $scope.close_menu = function (index) {
                 if (index !== undefined) {
                     $scope.activeView = $scope.menus[index];
                     $location.path($scope.activeView.title.replace(" ", "-"));
@@ -135,13 +188,13 @@
                 transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
                 docscroll = 0,
                 clickevent = mobilecheck() ? 'touchstart' : 'click';
-            $scope.menu_init = function() {
+            $scope.menu_init = function () {
                 var showMenu = document.getElementById( 'showMenu' ),
                     perspectiveWrapper = document.getElementById( 'perspective' ),
                     container = perspectiveWrapper.querySelector( '.container' ),
                     contentWrapper = container.querySelector( '.wrapper' );
 
-                showMenu.addEventListener( clickevent, function( ev ) {
+                showMenu.addEventListener( clickevent, function ( ev ) {
                     ev.stopPropagation();
                     ev.preventDefault();
                     docscroll = scrollY();
@@ -152,12 +205,12 @@
                     // add modalview class
                     classie.add( perspectiveWrapper, 'modalview' );
                     // animate..
-                    setTimeout( function() { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
+                    setTimeout( function () { classie.add( perspectiveWrapper, 'animate' ); }, 25 );
                 });
 
-                container.addEventListener( clickevent, function( ev ) {
+                container.addEventListener( clickevent, function ( ev ) {
                     if( classie.has( perspectiveWrapper, 'animate') ) {
-                        var onEndTransFn = function( ev ) {
+                        var onEndTransFn = function ( ev ) {
                             if( support && ( ev.target.className !== 'container' || ev.propertyName.indexOf( 'transform' ) == -1 ) ) return;
                             this.removeEventListener( transEndEventName, onEndTransFn );
                             classie.remove( perspectiveWrapper, 'modalview' );
@@ -172,17 +225,17 @@
                         else {
                             onEndTransFn.call();
                         }
-                        setTimeout(function(){
+                        setTimeout(function () {
                             classie.remove( perspectiveWrapper, 'modalview' );
                         }, 800);
-                        setTimeout(function() {
+                        setTimeout(function () {
                             classie.remove( perspectiveWrapper, 'animate' );
                         }, 400);
                     }
                 });
-                perspectiveWrapper.addEventListener( clickevent, function( ev ) {
+                perspectiveWrapper.addEventListener( clickevent, function ( ev ) {
                     if( classie.has( perspectiveWrapper, 'animate') ) {
-                        var onEndTransFn = function( ev ) {
+                        var onEndTransFn = function ( ev ) {
                             if( support && ( ev.target.className !== 'container' || ev.propertyName.indexOf( 'transform' ) == -1 ) ) return;
                             this.removeEventListener( transEndEventName, onEndTransFn );
                             classie.remove( perspectiveWrapper, 'modalview' );
@@ -197,10 +250,10 @@
                         else {
                             onEndTransFn.call();
                         }
-                        setTimeout(function(){
+                        setTimeout(function () {
                             classie.remove( perspectiveWrapper, 'modalview' );
                         }, 800);
-                        setTimeout(function() {
+                        setTimeout(function () {
                             classie.remove( perspectiveWrapper, 'animate' );
                         }, 400);
                     }
