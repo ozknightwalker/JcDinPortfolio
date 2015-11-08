7361ab524c3db6a8cf44da534fafb660d999588d
ozknightwalker <ozknightwalker62@gmail.com>
2015-11-09 07:09:23 +0800

    remove static root

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -111,7 +111,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
