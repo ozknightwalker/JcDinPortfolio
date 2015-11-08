2756ce8442f7756917735b1a952ec791f09d9863
ozknightwalker <ozknightwalker62@gmail.com>
2015-11-08 10:26:00 +0800

    forgot to change something

--- Portfolio/wsgi.py
+++ Portfolio/wsgi.py
@@ -5,5 +5,5 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Portfolio.settings")
 
-# application = Cling(get_wsgi_application())
+application = Cling(get_wsgi_application())
 # application = get_wsgi_application()
