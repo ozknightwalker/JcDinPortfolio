ce6e4175c31134698b4f526070944ebff96105fd
Jc Din <ozknightwalker62@gmail.com>
2016-05-30 10:47:09 +0800

    require https

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -91,6 +91,7 @@ DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 
 # Internationalization
