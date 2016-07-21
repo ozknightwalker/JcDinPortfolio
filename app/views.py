dbd76e164d334ce3fbf070fcc1c5c1319c5c8cda
Jc Din <ozknightwalker62@gmail.com>
2016-07-21 08:43:53 +0800

    setup gzip in portfolio

--- app/views.py
+++ app/views.py
@@ -1,38 +1,41 @@
 from django.shortcuts import redirect, render_to_response
 from django.views import generic
+from django.views.decorators.gzip import GZipMiddleware
 from django.template import RequestContext
 
 
-class MainView(generic.TemplateView):
+class MainView(GZipMiddleware, generic.TemplateView):
     template_name = 'MainView.html'
 
     def dispatch(self, request, *args, **kwargs):
         return redirect('Portfolio_main')
 
 
-class TestView(generic.TemplateView):
+class TestView(GZipMiddleware, generic.TemplateView):
     template_name = 'TestingView.html'
 
 
-class ProfileView(generic.TemplateView):
+class ProfileView(GZipMiddleware, generic.TemplateView):
     template_name = 'ProfileView.html'
 
 
-class ExperienceView(generic.TemplateView):
+class ExperienceView(GZipMiddleware, generic.TemplateView):
     template_name = 'ExperienceView.html'
 
 
-class PowerView(generic.TemplateView):
+class PowerView(GZipMiddleware, generic.TemplateView):
     template_name = 'PowerView.html'
 
 
-class WorkView(generic.TemplateView):
+class WorkView(GZipMiddleware, generic.TemplateView):
     template_name = 'WorkView.html'
 
 
 def Custom500(request, template_name='500.html'):
-    return render_to_response(template_name, context_instance=RequestContext(request))
+    return render_to_response(template_name,
+                              context_instance=RequestContext(request))
 
 
 def Custom400(request, template_name='404.html'):
-    return render_to_response(template_name, context_instance=RequestContext(request))
+    return render_to_response(template_name,
+                              context_instance=RequestContext(request))
